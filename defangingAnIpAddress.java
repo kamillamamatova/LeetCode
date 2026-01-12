a863ce5f0bc95fea36c445389d7fcff5d2237200
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2026-01-12 00:40:02 -0500

    Defanging an IP Address on LeetCode

---
+++ defangingAnIpAddress.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        // Will store the new string
+        String newAddress = new String();
+
+        // Index for the newAddress string,
+        // since [.] is 3 characters and . is 1
+        int j = 0;
+        for(int i = 0; i < address.length(); i++){
+            if(address.charAt(i) == '.'){
+                newAddress += "[.]";
+                // Incremementing j by 2 since the length of [] is 2
+                j += 2;
+            }
+            else{
+                // If it's not '.' then just add it
+                newAddress += address.charAt(i);
+            }
+            j++;
+        }
+
+        return newAddress;
+
+        /*
+        - Given: String address
+        - Goal: Replace every . with [.]
+        */
+    }
+}
