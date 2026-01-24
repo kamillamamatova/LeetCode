715462b99ac808aed401e016f76f531e5edacb88
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2026-01-23 20:13:06 -0500

    Add Two Integers on LeetCode

---
+++ addTwoIntegers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        int sum = num1 + num2;
+        
+        return sum;
+
+        /*
+        - Given: 2 ints num1 & num2
+        - Return: sum of 2 ints
+        */
+    }
+}
